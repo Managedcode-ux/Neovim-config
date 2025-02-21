d497ac981a9c5b770a11d4d822b2a61bde59a36a
Managedcode-ux <71963227+Managedcode-ux@users.noreply.github.com>
2025-02-21 13:44:58 +0530

    Added the :Config command to quickly access the neovim configurations

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -5,12 +5,21 @@ vim.cmd("set shiftwidth=4")
 vim.opt.number = true
 vim.opt.clipboard = "unnamedplus"
 
-vim.opt.hlsearch = true    -- Enable search highlighting
-vim.opt.incsearch = true   -- Enable incremental search
-vim.opt.ignorecase = true  -- Ignore case in search patterns
-vim.opt.smartcase = true   -- Override ignorecase if search contains uppercase
+vim.opt.hlsearch = true -- Enable search highlighting
+vim.opt.incsearch = true -- Enable incremental search
+vim.opt.ignorecase = true -- Ignore case in search patterns
+vim.opt.smartcase = true -- Override ignorecase if search contains uppercase
 vim.opt.wrap = false
 
-vim.opt.termguicolors = true  -- Enable true color support
+vim.opt.termguicolors = true -- Enable true color support
 vim.opt.shell = "pwsh"
 vim.opt.shellcmdflag = "-NoLogo -Command"
+
+-- :Config opens the neovim config directory
+vim.api.nvim_create_user_command("Config", function()
+	local config_dir = vim.fn.stdpath("config")
+	vim.cmd("cd " .. config_dir)
+	vim.cmd("edit " .. config_dir)
+end, {
+	desc = "Open Neovim config directory",
+})
