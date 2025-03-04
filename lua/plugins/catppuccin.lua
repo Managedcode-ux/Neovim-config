792edcbfa796d5c3fa14a3704596890bf268ae05
Managedcode-ux <71963227+Managedcode-ux@users.noreply.github.com>
2025-03-04 12:04:48 +0530

    commited the catppuccin theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,12 +1,13 @@
-return { 
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    priority = 1000,
-    config = function()
-        -- require("catppuccin").setup({flavour = "latte"})
-        require("catppuccin").setup({
-            transparent_background = false,
-        })
-        vim.cmd.colorscheme "catppuccin"
-    end
-}
+-- return { 
+--     "catppuccin/nvim", 
+--     name = "catppuccin", 
+--     priority = 1000,
+--     config = function()
+--         -- require("catppuccin").setup({flavour = "latte"})
+--         require("catppuccin").setup({
+--             transparent_background = false,
+--         })
+--         vim.cmd.colorscheme "catppuccin"
+--     end
+-- }
+return {}
